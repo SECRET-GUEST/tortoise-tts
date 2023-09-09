42921e74394ff4069dbded363a0b6b20f5d5beaa
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-09-09 08:08:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         'unidecode',
         'scipy',
         'librosa',
-        #'transformers==4.31.0',
+        'transformers==4.31.0',
         'tokenizers',
     ],
     classifiers=[
